12783f5b5d1f6290c216b1d0efb66dd9f47dea36
Aayush Adlakha <aayushadlakha15@gmail.com>
2026-01-31 20:08:09 +0000

    Decode Png header

--- src/png_decoder.cpp
+++ src/png_decoder.cpp
@@ -1,5 +1,7 @@
 #include "png_decoder.h"
+#include <cstring>
 #include <iostream>
+#include <vector>
 
 bool PngDecoder::canDecode(const std::vector<uint8_t> &buff) const {
   if (buff.size() < 8) {
@@ -33,8 +35,7 @@ bool PngDecoder::canDecode(const std::vector<uint8_t> &buff) const {
 }
 
 std::optional<uint32_t>
-PngDecoder::read4BytesAsU32(const std::vector<uint8_t> &buff,
-                            int &idx) const {
+PngDecoder::read4BytesAsU32(const std::vector<uint8_t> &buff, int &idx) const {
   uint32_t ans = 0;
   if (idx + 4 > (int)buff.size()) {
     std::cerr << "Unexpected End of file...\n";
@@ -52,8 +53,7 @@ PngDecoder::read4BytesAsU32(const std::vector<uint8_t> &buff,
 }
 
 std::optional<std::string>
-PngDecoder::read4BytesAsStr(const std::vector<uint8_t> &buff,
-                            int &idx) const {
+PngDecoder::read4BytesAsStr(const std::vector<uint8_t> &buff, int &idx) const {
   std::string ans;
   if (idx + 4 > (int)buff.size()) {
     std::cerr << "Unexpected End of file...\n";
@@ -70,12 +70,51 @@ PngDecoder::read4BytesAsStr(const std::vector<uint8_t> &buff,
   return ans;
 }
 
-bool PngDecoder::parsePng(const std::vector<uint8_t> &buff) const {
+PngDecoder::Ihdr PngDecoder::parseIhdr(const std::vector<uint8_t> &buff,
+                                       int &idx) const {
+  Ihdr header{};
+  for (int i = idx; i < idx + 4; ++i) {
+    header.width = (header.width << 8) | (uint32_t)buff[i];
+  }
+  std::cout << "Width: " << header.width << "\n";
+  idx += 4;
+
+  for (int i = idx; i < idx + 4; ++i) {
+    header.height = (header.height << 8) | (uint32_t)buff[i];
+  }
+  std::cout << "Height: " << header.height << "\n";
+  idx += 4;
+
+  std::memcpy(&header.bitDepth, &buff[idx], 1);
+  std::cout << "BitDepth: " << (uint32_t)header.bitDepth << "\n";
+  idx += 1;
+
+  std::memcpy(&header.colorType, &buff[idx], 1);
+  std::cout << "ColorType: " << (uint32_t)header.colorType << "\n";
+  idx += 1;
+
+  std::memcpy(&header.compMethod, &buff[idx], 1);
+  std::cout << "CompressionMethod: " << (uint32_t)header.compMethod << "\n";
+  idx += 1;
+
+  std::memcpy(&header.filterMethod, &buff[idx], 1);
+  std::cout << "FilterMethod: " << (uint32_t)header.filterMethod << "\n";
+  idx += 1;
+
+  std::memcpy(&header.interlceMethod, &buff[idx], 1);
+  std::cout << "InterlaceMethod: " << (uint32_t)header.interlceMethod << "\n";
+  idx += 1;
+
+  return header;
+}
+
+bool PngDecoder::parsePng(const std::vector<uint8_t> &buff, Image &out) const {
   std::cout << "Found Png...\n";
   int idx = 8;
+  Ihdr header;
 
   while (true) {
-    std::cout << "Reading Chunk...\n";
+    std::cout << "\n";
     auto maybeLen = read4BytesAsU32(buff, idx);
 
     if (!maybeLen.has_value()) {
@@ -94,7 +133,14 @@ bool PngDecoder::parsePng(const std::vector<uint8_t> &buff) const {
     std::string chunkType = maybeChunkType.value();
     std::cout << "ChunkType: " << chunkType << "\n";
 
-    idx += len;
+    if (chunkType == "IHDR") {
+      header = parseIhdr(buff, idx);
+      out.setDimensions(header.width, header.height);
+      out.reservePixels(static_cast<size_t>(header.width) * header.height * 4);
+
+    } else {
+      idx += len;
+    }
 
     auto maybeCrc = read4BytesAsU32(buff, idx);
 
@@ -116,5 +162,5 @@ bool PngDecoder::parsePng(const std::vector<uint8_t> &buff) const {
 bool PngDecoder::decode(const std::vector<uint8_t> &buff, Image &out) const {
   out.setDimensions(0, 0);
   out.reservePixels(0);
-  return parsePng(buff);
+  return parsePng(buff, out);
 }
--- src/png_decoder.h
+++ src/png_decoder.h
@@ -1,9 +1,10 @@
 #pragma once
+#include "image.h"
+#include "image_decoder.h"
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <vector>
-#include "image.h"
-#include "image_decoder.h"
 
 class PngDecoder final : public ImageDecoder {
 public:
@@ -11,7 +12,17 @@ public:
   bool decode(const std::vector<uint8_t> &buff, Image &out) const override;
 
 private:
-  bool parsePng(const std::vector<uint8_t> &buff) const;
+  struct Ihdr {
+    uint32_t width;
+    uint32_t height;
+    uint8_t bitDepth;
+    uint8_t colorType;
+    uint8_t compMethod;
+    uint8_t filterMethod;
+    uint8_t interlceMethod;
+  };
+  bool parsePng(const std::vector<uint8_t> &buff, Image &out) const;
+  Ihdr parseIhdr(const std::vector<uint8_t> &buff, int &idx) const;
   std::optional<uint32_t> read4BytesAsU32(const std::vector<uint8_t> &buff,
                                           int &idx) const;
   std::optional<std::string> read4BytesAsStr(const std::vector<uint8_t> &buff,
