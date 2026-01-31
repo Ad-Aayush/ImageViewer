050eab0300decb77b3f8b1d87ac5c595fd95ce1d
Aayush Adlakha <aayushadlakha15@gmail.com>
2026-01-31 11:13:42 +0000

    Add boilerplate for PNG

--- image_viewer.cpp
+++ image_viewer.cpp
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <iostream>
 #include <optional>
+#include <string>
 #include <vector>
 
 extern "C" {
@@ -86,6 +87,9 @@ private:
   int m_width;
   std::vector<uint8_t> m_pixels;
 
+  bool isPng(std::vector<uint8_t> &buff);
+  bool parsePng(std::vector<uint8_t> &buff);
+
   bool isQoi(std::vector<uint8_t> &buff);
   bool parseQoi(std::vector<uint8_t> &buff);
   QoiHeader parseQoiHeader(std::vector<uint8_t> &buff);
@@ -159,6 +163,116 @@ bool Image::isQoi(std::vector<uint8_t> &buff) {
   for (int i = sz - 2; i >= sz - 8; i--) {
     if (buff[i] != 0x00) {
       return false;
+    }
+  }
+
+  return true;
+}
+
+// 89 50 4E 47 0D 0A 1A 0A
+bool Image::isPng(std::vector<uint8_t> &buff) {
+  if (buff.size() < 8) {
+    return false;
+  }
+  if (buff[0] != 0x89) {
+    return false;
+  }
+  if (buff[1] != 0x50) {
+    return false;
+  }
+  if (buff[2] != 0x4E) {
+    return false;
+  }
+  if (buff[3] != 0x47) {
+    return false;
+  }
+  if (buff[4] != 0x0D) {
+    return false;
+  }
+  if (buff[5] != 0x0A) {
+    return false;
+  }
+  if (buff[6] != 0x1A) {
+    return false;
+  }
+  if (buff[7] != 0x0A) {
+    return false;
+  }
+  return true;
+}
+
+std::optional<uint32_t> read4BytesAsU32(const std::vector<uint8_t> &buff, int &idx) {
+  uint32_t ans = 0;
+  if (idx + 4 > buff.size()) {
+    std::cerr << "Unexpected End of file...\n";
+    return std::nullopt;
+  }
+
+  int cnt = 0;
+
+  while (cnt < 4) {
+    ans = (ans << 8) | (uint32_t)buff[idx++];
+    cnt++;
+  }
+
+  return ans;
+}
+
+std::optional<std::string> read4BytesAsStr(const std::vector<uint8_t> &buff, int &idx) {
+  std::string ans;
+  if (idx + 4 > buff.size()) {
+    std::cerr << "Unexpected End of file...\n";
+    return std::nullopt;
+  }
+
+  int cnt = 0;
+
+  while (cnt < 4) {
+    ans += (char)buff[idx++];
+    cnt++;
+  }
+
+  return ans;
+}
+
+bool Image::parsePng(std::vector<uint8_t> &buff) {
+  std::cout << "Found Png...\n";
+  int idx = 8;
+
+  while (true) {
+    std::cout << "Reading Chunk...\n";
+    auto maybeLen = read4BytesAsU32(buff, idx);
+
+    if (!maybeLen.has_value()) {
+      return false;
+    }
+
+    uint32_t len = maybeLen.value();
+    std::cout << "Length: " << len << "\n";
+
+    auto maybeChunkType = read4BytesAsStr(buff, idx);
+
+    if (!maybeChunkType.has_value()) {
+      return false;
+    }
+
+    std::string chunkType = maybeChunkType.value();
+    std::cout << "ChunkType: " << chunkType << "\n";
+
+    idx += len;
+
+    auto maybeCrc = read4BytesAsU32(buff, idx);
+
+    if (!maybeCrc .has_value()) {
+      return false;
+    }
+
+    uint32_t crc = maybeCrc.value();
+    std::cout << "Crc: " << crc << "\n";
+
+
+    if (chunkType == "IEND") {
+      break;
     }
   }
 
@@ -801,6 +915,10 @@ bool Image::load(const std::string &file) {
 
   if (isQoi(byteBuf)) {
     return parseQoi(byteBuf);
+  }
+
+  if (isPng(byteBuf)) {
+    return parsePng(byteBuf);
   }
   return false;
 }
